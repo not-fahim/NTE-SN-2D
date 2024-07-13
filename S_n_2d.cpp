7c9302d02c066acfb0e2c5cdfc7762eca9ea4eb1
Md. Fahim Foraejee <mdfahimforaejee@gmail.com>
2024-07-14 03:03:21 +0600

    maybe the transport sweep is working , yay

--- S_n_2d.cpp
+++ S_n_2d.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<vector>
 #include<cmath>
+#include<iomanip>
 
 using namespace std;
 
@@ -33,7 +34,7 @@ class angular
     int total_num;
     angular()
     {
-        vector<int>sequence{1,
+        vector<int>sequence1{1,
                             2, 2, 
                             3, 5, 3, 
                             4, 6, 6, 4, 
@@ -41,10 +42,14 @@ class angular
                             3, 6, 8, 8, 6, 3, 
                             2, 5, 6, 7, 6, 5, 2, 
                             1, 2, 3, 4, 4, 3, 2, 1 };
+        
+        sequence = sequence1;
         for (int i=0; i<size(sequence);i++)
             sequence[i]=sequence[i]-1;
+        
         total_num = size(sequence);
-        vector<double>mu{   0.1389568, 
+
+        vector<double>mu1{   0.1389568, 
                             0.3922893,
                             0.5370966, 
                             0.6504264, 
@@ -52,8 +57,8 @@ class angular
                             0.8319966, 
                             0.9092855, 
                             0.9805009};
-
-        vector<double>w={   0.0489872, 
+        mu = mu1;
+        vector<double>w1={   0.0489872, 
                             0.0413296, 
                             0.0212326, 
                             0.0256207, 
@@ -61,6 +66,7 @@ class angular
                             0.0144589, 
                             0.0344958,
                             0.0085179 };
+        w=w1;
     }    
 };
 
@@ -73,8 +79,9 @@ class geometry_data
     double sigma_t= 1;
     double sigma_s = 0.7;
     double nu_sigma_f = 0.39;
+    int albedo[4] = {0,0,0,0};
 
-    int Nx = 10, Ny = 10;
+    int Nx = 8, Ny = 10;
 
     double del_x = X/ double(Nx), del_y = Y/double(Ny);
     vector<double> x_i_minus_half, x_i_plus_half, x_i ;
@@ -84,13 +91,13 @@ class geometry_data
 
     geometry_data()
     {
-        vector<double> x_i_minus_half = range(0,X-del_x,Nx);
-        vector<double> x_i_plus_half = range(del_x, X, Nx);
-        vector<double> x_i = cell_midpoints(x_i_minus_half, x_i_plus_half);
+        x_i_minus_half = range(0,X-del_x,Nx);
+        x_i_plus_half = range(del_x, X, Nx);
+        x_i = cell_midpoints(x_i_minus_half, x_i_plus_half);
 
-        vector<double> y_j_minus_half = range(0,Y-del_y,Ny);
-        vector<double> y_j_plus_half = range(del_y, Y, Ny);
-        vector<double> y_j = cell_midpoints(y_j_minus_half,x_i_plus_half);   
+        y_j_minus_half = range(0,Y-del_y,Ny);
+        y_j_plus_half = range(del_y, Y, Ny);
+        y_j = cell_midpoints(y_j_minus_half,x_i_plus_half);   
 
 
 
@@ -99,10 +106,18 @@ class geometry_data
 };
 
 
-class boundary_flux
+class boundary_flux_class
 {
     public:
-    vector<vector<vector<double>>> bottom, right, top, left ;
+    vector<vector<vector<vector<double>>>> bottom_top, left_right;
+    boundary_flux_class(int xpoints,int ypoints, int angles)
+    {
+        vector<vector<vector<vector<double>>>> bottom_top1(2, vector<vector<vector<double>>>(4, vector<vector<double>> (xpoints, vector<double> (angles, 0))));
+        bottom_top = bottom_top1;
+
+        vector<vector<vector<vector<double>>>> left_right1(2, vector<vector<vector<double>>>(4, vector<vector<double>> (ypoints, vector<double> (angles, 0))));
+        left_right = left_right1;
+    }
     
 };
 
@@ -111,60 +126,76 @@ class sweep_direction_class
     //this class provides the set of data required for setting direction for sweeps. there can be left to right, bottom to top and any order for both the axes
     //for example sweep('L', 'B', geometry) will sweep from left to right and bottom to top
     public:
-    int x_start, x_end, y_start, y_end , x_dir, y_dir;
-    int Nx, Ny, V_reflect, H_reflect;
+    int x_start, x_end, y_start, y_end , x_dir, y_dir, V_boun_start, H_boun_start;
+    int Nx, Ny, H_reflect, V_reflect, H_in, V_in;
 
-    sweep_direction_class(char x_first, char y_first, geometry_data* geometry)
+    void set_sweep_direction(char x_first, char y_first, geometry_data* geometry)
     {
         Nx = geometry->Nx;
         Ny = geometry->Ny;
-
+        int L=0, R=1;
+        int B=0, T=1;
         
         //x axis
-        if (x_first = 'L' )
+        if (x_first == 'L' )
         {
             x_start = 0;
-            x_end = Nx;
+            x_end = Nx-1;
             x_dir = 1;
+            V_boun_start = L;
 
-            if (y_first = 'B' )
+            if (y_first == 'B' )
             {
                 y_start = 0;
-                y_end =0;
+                y_end =Ny-1;
                 y_dir = 1;
-                V_reflect = 1;
+                H_in = 0;
                 H_reflect = 3;
+                V_in = 0;
+                V_reflect = 1;
+                H_boun_start = B;
+
             }
-            if (y_first = 'T' )
+            if (y_first == 'T' )
             {
-                y_start = geometry->Ny;
+                y_start = geometry->Ny-1;
                 y_end = 0;
                 y_dir = -1;
-                V_reflect = 2;
+                H_in = 3;
                 H_reflect = 0;
+                V_in = 3;
+                V_reflect = 2;
+                H_boun_start = T;
             }
         }
-        if (x_first = 'R' )
+        if (x_first == 'R' )
         {
-            x_start = geometry->Nx;
+            x_start = geometry->Nx-1;
             x_end = 0;
             x_dir = -1;
+            V_boun_start = R;
 
-            if (y_first = 'B' )
+            if (y_first == 'B' )
             {
                 y_start = 0;
-                y_end =0;
+                y_end =Ny-1;
                 y_dir = 1;
-                V_reflect = 0;
+                H_in = 1;
                 H_reflect = 2;
+                V_in = 1;
+                V_reflect = 0;
+                H_boun_start = B;
             }
-            if (y_first = 'T' )
+            if (y_first == 'T' )
             {
-                y_start = geometry->Ny;
+                y_start = geometry->Ny-1;
                 y_end = 0;
                 y_dir = -1;
-                V_reflect = 3;
+                H_in = 2;
                 H_reflect = 1;
+                V_in = 2;
+                V_reflect = 3;
+                H_boun_start = T;
             }
         }
 
@@ -183,53 +214,87 @@ class sweeper_class{
     vector<vector<double>> psi_ij;
     vector<vector<double>> flux_ij;
     vector<vector<double>> Q;
-    vector<vector<vector<double>>> psi_boun_B, psi_boun_R, psi_boun_T, psi_boun_L ;
+    vector<vector<vector<vector<double>>>> bottom_top, left_right;
+    
     sweeper_class(geometry_data geometry1, angular angle1)
     {
         geometry = geometry1;
         angle = angle1;
-        vector<vector<double>> psi_ij(2*geometry.Nx+1, vector<double> (2*geometry.Ny+1,0));
+        vector<vector<double>> psi_ij1(2*geometry.Nx+1, vector<double> (2*geometry.Ny+1,0));
+        psi_ij = psi_ij1;
+
+        
+        vector<vector<vector<vector<double>>>> bottom_top1(2, vector<vector<vector<double>>>(4, vector<vector<double>> (geometry.Nx, vector<double> (angle.total_num, 0))));
+        bottom_top = bottom_top1;
+
+        vector<vector<vector<vector<double>>>> left_right1(2, vector<vector<vector<double>>>(4, vector<vector<double>> (geometry.Ny, vector<double> (angle.total_num, 0))));
+        left_right = left_right1;
     }
 
-    int transport_sweep(vector<vector<double>> q, vector<vector<double>> flux)
+    vector<vector<double>> transport_sweep(vector<vector<double>> q, vector<vector<double>> flux)
     {
         flux_ij = flux;
         Q = q;
-        int n;
-        for (n = 0; n< (angle.total_num); n++);
+        
+        sweep_direction_class sweep_dir;
+        
+        for (int n = 0; n<angle.total_num; n++)
         {
-            sweep_direction_class sweep_dir('L', 'B', &geometry);
+            sweep_dir.set_sweep_direction('L', 'B', &geometry);
             sweep_part(sweep_dir, n, Q);
 
-            sweep_direction_class sweep_dir('R', 'B', &geometry);
+            sweep_dir.set_sweep_direction('R', 'B', &geometry);
             sweep_part(sweep_dir, n, Q);
 
-            sweep_direction_class sweep_dir('L', 'T', &geometry);
+            sweep_dir.set_sweep_direction('L', 'T', &geometry);
             sweep_part(sweep_dir, n, Q);
 
-            sweep_direction_class sweep_dir('R', 'T', &geometry);
+            sweep_dir.set_sweep_direction('R', 'T', &geometry);
             sweep_part(sweep_dir, n, Q);
         }
-        return 0;
+        return flux_ij;
     }
 
 
-    int sweep_part(sweep_direction_class sweep_dir, int n, vector<vector<double>> Q)
+    void sweep_part(sweep_direction_class sweep_dir, int n, vector<vector<double>> Q)
     {
+        
         int cell_x, cell_y;
         for(int i=sweep_dir.x_start; 0<=i && i<sweep_dir.Nx; i=i+sweep_dir.x_dir)
-        {
+        {   
             cell_x = 2*i+1;
             for(int j=sweep_dir.y_start; 0<=j && j<sweep_dir.Ny;j+= sweep_dir.y_dir)
             {
                 cell_y = 2*j+1;
-                psi_ij[cell_x][cell_y] = (geometry.sigma_t + 2*angle.mu[n]/geometry.del_x + 2*angle.mu[n]/geometry.del_y)*(2*angle.mu[n]/geometry.del_x * psi_ij[cell_x-sweep_dir.x_dir][cell_y] + 2*angle.mu[n]/geometry.del_y*psi_ij[cell_x][cell_y-sweep_dir.y_dir]+1/(4*M_PI)*Q[cell_x][cell_y]) ;
-                psi_ij[cell_x+sweep_dir.x_dir][cell_y] = 2*psi_ij[cell_x][cell_y] - psi_ij[cell_x-sweep_dir.x_dir][cell_y];
-                psi_ij[cell_x][cell_y+sweep_dir.y_dir] = 2*psi_ij[cell_x][cell_y] - psi_ij[cell_x][cell_y-sweep_dir.y_dir];
-                flux_ij[i][j] = flux_ij[i][j] + (1/4)*angle.w[n]*psi_ij[cell_x][cell_y];
-            }
+                if(i==sweep_dir.x_start) //vertical boundary
+                {
+                    psi_ij[cell_x-sweep_dir.x_dir][cell_y] = left_right[sweep_dir.V_boun_start][sweep_dir.V_in][j][n];
+                }
+                if(j==sweep_dir.y_start) //horizontal boundary
+                {
+                    psi_ij[cell_x][cell_y - sweep_dir.y_dir] = bottom_top[sweep_dir.H_boun_start][sweep_dir.H_in][i][n];
+                }
+                psi_ij[cell_x][cell_y] = 1.0/(geometry.sigma_t + 2.0*angle.mu[angle.sequence[n]]/geometry.del_x + 2.0*angle.mu[angle.sequence[n]]/geometry.del_y)*(2.0*angle.mu[angle.sequence[n]]/geometry.del_x * psi_ij[cell_x-sweep_dir.x_dir][cell_y] + 2.0*angle.mu[angle.sequence[n]]/geometry.del_y*psi_ij[cell_x][cell_y-sweep_dir.y_dir]+1.0/(4.0*M_PI)*Q[i][j]) ;
+                psi_ij[cell_x+sweep_dir.x_dir][cell_y] = 2.0*psi_ij[cell_x][cell_y] - psi_ij[cell_x-sweep_dir.x_dir][cell_y];
+                psi_ij[cell_x][cell_y+sweep_dir.y_dir] = 2.0*psi_ij[cell_x][cell_y] - psi_ij[cell_x][cell_y-sweep_dir.y_dir];
+                
+                flux_ij[i][j] = flux_ij[i][j] + (1.0/4.0)*angle.w[angle.sequence[n]]*psi_ij[cell_x][cell_y];
+
+                if(i==sweep_dir.x_end) //vertical boundary
+                {
+                    left_right[abs(1-sweep_dir.V_boun_start)][sweep_dir.V_reflect][j][n] = 0*psi_ij[cell_x+sweep_dir.x_dir][cell_y];
+                }
+                if(j==sweep_dir.y_end) //horizontal boundary
+                {
+                    bottom_top[abs(1-sweep_dir.H_boun_start)][sweep_dir.H_reflect][i][n] = 0*psi_ij[cell_x][cell_y+sweep_dir.y_dir];
+                }                 
             
+
+
+            }
+
         }
+        
     }
 };
 
@@ -241,14 +306,47 @@ int main()
     //angular discretization
     angular angle;
     geometry_data geometry;
-    vector<vector<double>> flux(geometry.Nx,vector<double> (geometry.Ny,0));
-    vector<vector<double>> flux_old(geometry.Nx,vector<double> (geometry.Ny,0));
-    vector<vector<double>> q(geometry.Nx,vector<double> (geometry.Ny,0));
-
     
+    sweeper_class sweep_object(geometry, angle);
+    vector<vector<double>> flux_old(geometry.Nx, vector<double> (geometry.Ny,1));
+    vector<vector<double>> q(geometry.Nx, vector<double> (geometry.Ny,0));
+    vector<vector<double>> fission_source = q, fission_source_new;
+    for(int i=0; i<geometry.Nx; i++)
+    {
+        for (int j=0; j<geometry.Ny; j++)
+        {
+            fission_source[i][j] = flux_old[i][j] * geometry.nu_sigma_f;
+            q[i][j] = (fission_source[i][j]+flux_old[i][j]*geometry.sigma_s); 
+        }
+        
+    }
+
+    sweep_object.transport_sweep(q, flux_old);
+    for(int i=0; i<geometry.Nx; i++)
+    {
+        for (int j=0; j<geometry.Ny; j++)
+        {
+            fission_source[i][j] = sweep_object.flux_ij[i][j] * geometry.nu_sigma_f;
+            q[i][j] = (fission_source[i][j]+sweep_object.flux_ij[i][j]*geometry.sigma_s); 
+        }
+        
+    }
+
+    sweep_object.transport_sweep(q, sweep_object.flux_ij);
 
 
 
-    
+    for(int i=0; i<geometry.Nx; i++)
+    {
+        for(int j=0; j<geometry.Ny; j++)
+        {
+            cout<<sweep_object.flux_ij[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+
 
 }
---
+++ test2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    vector<vector<vector<vector<double>>>> bottom_top(2, vector<vector<vector<double>>> (4, vector<vector<double>> (10, vector<double> (3,13))));
+    cout <<(bottom_top[1][2][2][2]);
+    
+}
